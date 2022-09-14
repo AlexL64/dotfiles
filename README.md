fde0357656f8e9e8c61648c7597e6c4a41ae239a
Alex <alex.lucarin@gmail.com>
2022-09-14 22:41:53 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dotfiles
+My linux installations dotfiles
---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+starship init fish | source
+
+
+
+
+
+# My aliases
+
+alias bw_search='bw list items --pretty --search'
