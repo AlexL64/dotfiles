8e2b3cd5417fb3d858d9751464502fd2d7482e7e
Alex <alex.lucarin@gmail.com>
2022-09-20 18:46:55 +0200

    fixed xautolock timer

--- awesome/main/autostart.lua
+++ awesome/main/autostart.lua
@@ -2,4 +2,4 @@ local awful = require("awful")
 
 awful.spawn.with_shell("picom --experimental-backends")
 awful.spawn.with_shell("xss-lock -- bash ~/.config/awesome/scripts/lockscreen.sh --nofork")
-awful.spawn.with_shell("xautolock -locker 'bash ~/.config/awesome/scripts/lockscreen.sh' -time 1")
+awful.spawn.with_shell("xautolock -locker 'bash ~/.config/awesome/scripts/lockscreen.sh' -time 10")
