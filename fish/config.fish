6bf83aeb2410388f7cb81ed208b63fe7c2d19b85
AlexL64 <alex.lucarin@protonmail.com>
2024-02-11 15:36:26 +0100

    base hyrpland + base waybar + kitty

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+
+
+alias 'pacman_clear'='yay -Qtdq | yay -Rns -'
+alias 'pacman_clear_show_all'='yay -Qqd | yay -Rsu --print -'
+alias 'pacman_clear_all'='yay -Qqd | yay -Rsu -'
