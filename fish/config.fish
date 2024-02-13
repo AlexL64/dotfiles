81d5b7e22df3e1723dde3d06d8a9d5d2202168df
AlexL64 <alex.lucarin@protonmail.com>
2024-02-13 13:59:03 +0100

    Add alias for kitten ssh

--- fish/config.fish
+++ fish/config.fish
@@ -8,3 +8,4 @@ starship init fish | source
 alias 'pacman_clear'='yay -Qtdq | yay -Rns -'
 alias 'pacman_clear_show_all'='yay -Qqd | yay -Rsu --print -'
 alias 'pacman_clear_all'='yay -Qqd | yay -Rsu -'
+alias 'ssh'='kitten ssh'
