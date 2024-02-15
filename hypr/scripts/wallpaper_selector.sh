7c7debd17ad9bd16b3b6b9f9d86c84acbd7fc218
AlexL64 <alex.lucarin@protonmail.com>
2024-02-15 13:42:16 +0100

    Wallpaper selector + windows rules

--- hypr/scripts/wallpaper_selector.sh
+++ hypr/scripts/wallpaper_selector.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# current=$(swww query | sed '/^image:$/,$!d')
-# echo $current
-wallpaper=$(ls ~/Wallpaper/ | while read A ; do  echo -en "$A\x00icon\x1f~/Wallpaper/$A\n" ; done | rofi -show -dmenu -theme $HOME/.config/rofi/themes/style.rasi)
-
-# -select
+current=$(basename $(swww query | grep -oP '(?<=image:).*'))
+wallpaper=$(ls ~/Wallpaper/ | while read A ; do  echo -en "$A\x00icon\x1f~/Wallpaper/$A\n" ; done | rofi -dmenu -theme $HOME/.config/rofi/themes/wallpaper_selector_style.rasi -select "$current" -config ~/.config/rofi/config.rasi) 
 
 swww img ~/Wallpaper/$wallpaper --transition-type grow --transition-pos bottom-right
