e89a1f7b214f1c2f8fb1e22d74edb59639d64e86
AlexL64 <alex.lucarin@protonmail.com>
2024-02-16 16:42:39 +0100

    Improvement to secondary monitor config switching script

--- hypr/scripts/monitor_config.sh
+++ hypr/scripts/monitor_config.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
-lines="HDMI-A-1,preferred,auto,auto,mirror,eDP-1\nHDMI-A-1,preferred,auto,auto\nHDMI-A-1,preferred,auto,1\nHDMI-A-1,preferred,auto,1.33\nHDMI-A-1,preferred,auto,1.6\nHDMI-A-1,preferred,auto,2"
-config=$(printf $lines | rofi -dmenu -theme $HOME/.config/rofi/themes/monitor_config.rasi -p "Monitor 2 config")
+lines="Mirror\nScale Auto\nScale 1\nScale 1.33\nScale 1.6\nScale 2"
+config=$(echo -e $lines | rofi -dmenu -theme $HOME/.config/rofi/themes/monitor_config.rasi -p "Monitor 2 config")
+
+case $config in
+"Mirror")
+    config="HDMI-A-1,preferred,auto,auto,mirror,eDP-1"
+    ;;
+"Scale Auto")
+    config="HDMI-A-1,preferred,auto,auto"
+    ;;
+"Scale 1")
+    config="HDMI-A-1,preferred,auto,1"
+    ;;
+"Scale 1.33")
+    config="HDMI-A-1,preferred,auto,1.33"
+    ;;
+"Scale 1.6")
+    config="HDMI-A-1,preferred,auto,1.6"
+    ;;
+    "Scale 2")
+    config="HDMI-A-1,preferred,auto,2"
+    ;;
+*)
+    ;;
+esac
+
 hyprctl keyword monitor $config
