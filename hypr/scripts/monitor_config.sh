3c256500375101f32e94d731fd9e18c89abbb2f0
AlexL64 <github.qrfqa@slmails.com>
2024-02-16 16:06:29 +0100

    Window rules that fix steam for some reason...

--- hypr/scripts/monitor_config.sh
+++ hypr/scripts/monitor_config.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-lines="HDMI-A-1,preferred,auto,auto,mirror,eDP-1\nHDMI-A-1,preferred,auto,auto\nHDMI-A-1,preferred,auto,1\nHDMI-A-1,preferred,auto,1.3333333333\nHDMI-A-1,preferred,auto,1.6\nHDMI-A-1,preferred,auto,2"
+lines="HDMI-A-1,preferred,auto,auto,mirror,eDP-1\nHDMI-A-1,preferred,auto,auto\nHDMI-A-1,preferred,auto,1\nHDMI-A-1,preferred,auto,1.33\nHDMI-A-1,preferred,auto,1.6\nHDMI-A-1,preferred,auto,2"
 config=$(printf $lines | rofi -dmenu -theme $HOME/.config/rofi/themes/monitor_config.rasi -p "Monitor 2 config")
 hyprctl keyword monitor $config
