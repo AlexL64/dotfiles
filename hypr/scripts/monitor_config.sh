97102355b8d358d675b2487a0fc2a701df9a8fdf
AlexL64 <alex.lucarin@protonmail.com>
2024-02-16 16:54:16 +0100

    Add more 3 to the scale because without that things look blurry

--- hypr/scripts/monitor_config.sh
+++ hypr/scripts/monitor_config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-lines="Mirror\nScale Auto\nScale 1\nScale 1.33\nScale 1.6\nScale 2"
+lines="Mirror\nScale Auto\nScale 100%\nScale 133%\nScale 160%\nScale 200%"
 config=$(echo -e $lines | rofi -dmenu -theme $HOME/.config/rofi/themes/monitor_config.rasi -p "Monitor 2 config")
 
 case $config in
@@ -10,16 +10,16 @@ case $config in
 "Scale Auto")
     config="HDMI-A-1,preferred,auto,auto"
     ;;
-"Scale 1")
+"Scale 100%")
     config="HDMI-A-1,preferred,auto,1"
     ;;
-"Scale 1.33")
-    config="HDMI-A-1,preferred,auto,1.33"
+"Scale 133%")
+    config="HDMI-A-1,preferred,auto,1.3333333333"
     ;;
-"Scale 1.6")
+"Scale 160%")
     config="HDMI-A-1,preferred,auto,1.6"
     ;;
-    "Scale 2")
+    "Scale 200%")
     config="HDMI-A-1,preferred,auto,2"
     ;;
 *)
