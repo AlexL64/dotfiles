b845fee21799d803ff12866c64c9db661ec26fe8
AlexL64 <alex.lucarin@protonmail.com>
2024-02-17 16:30:16 +0100

    Small changes

--- hypr/scripts/wallpaper_selector.sh
+++ hypr/scripts/wallpaper_selector.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-current=$(basename $(swww query | grep -oP '(?<=image:).*'))
+current=$(basename $(swww query | grep -oP '(?<=image: ).*'))
 wallpaper=$(ls ~/Wallpaper/ | while read A ; do  echo -en "$A\x00icon\x1f~/Wallpaper/$A\n" ; done | rofi -dmenu -theme $HOME/.config/rofi/themes/wallpaper_selector_style.rasi -select "$current") 
 
 swww img ~/Wallpaper/$wallpaper --transition-type grow --transition-pos bottom-right
