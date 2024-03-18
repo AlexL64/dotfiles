45db5bf94e2a3faac14829bfff42b5714b1bd17a
AlexL64 <alex.lucarin@protonmail.com>
2024-03-18 10:34:43 +0100

    Changes to hyprlock

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,5 @@ alias 'pacman_clear'='yay -Qtdq | yay -Rns -'
 alias 'pacman_clear_show_all'='yay -Qqd | yay -Rsu --print -'
 alias 'pacman_clear_all'='yay -Qqd | yay -Rsu -'
 alias 'ssh'='kitten ssh'
+# Created by `pipx` on 2024-03-15 15:11:17
+set PATH $PATH /home/alex/.local/bin
