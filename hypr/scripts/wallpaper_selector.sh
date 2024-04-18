2d82710f9d1235d28e8f2f2da467cc27bd290f6d
AlexL64 <github.qrfqa@slmails.com>
2024-04-18 10:19:29 +0200

    Add randomness to wallpaper selector + changes to neofetch

--- hypr/scripts/wallpaper_selector.sh
+++ hypr/scripts/wallpaper_selector.sh
@@ -1,6 +1,55 @@
 #!/bin/sh
 
-current=$(basename $(swww query | grep -oP '(?<=image: ).*'))
-wallpaper=$(ls ~/Wallpaper/ | while read A ; do  echo -en "$A\x00icon\x1f~/Wallpaper/$A\n" ; done | rofi -dmenu -theme $HOME/.config/rofi/themes/wallpaper_selector_style.rasi -select "$current") 
+for i in {0..25}
+do
+  echo $(($RANDOM % 10))
+done
 
-swww img ~/Wallpaper/$wallpaper --transition-type grow --transition-pos bottom-right
+current=$(basename $(swww query | grep -oP '(?<=image: ).*'))
+wallpaper=$(ls ~/Wallpaper/ | while read A; do echo -en "$A\x00icon\x1f~/Wallpaper/$A\n"; done | rofi -dmenu -theme $HOME/.config/rofi/themes/wallpaper_selector_style.rasi -select "$current")
+
+case $(($RANDOM % 2)) in
+0)
+    type="grow"
+    ;;
+1)
+    type="outer"
+    ;;
+*) ;;
+esac
+
+case $(($RANDOM % 10)) in
+0)
+    pos="center"
+    ;;
+1)
+    pos="bottom"
+    ;;
+2)
+    pos="top"
+    ;;
+3)
+    pos="left"
+    ;;
+4)
+    pos="right"
+    ;;
+5)
+    pos="bottom-right"
+    ;;
+6)
+    pos="bottom-left"
+    ;;
+7)
+    pos="top-right"
+    ;;
+8)
+    pos="top-left"
+    ;;
+9)
+    pos="top-right"
+    ;;
+*) ;;
+esac
+
+swww img ~/Wallpaper/$wallpaper --transition-type $type --transition-pos $pos
