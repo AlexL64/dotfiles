891a6f3266d0793a14aa1082f84f434c1ed2bd20
AlexL64 <alex.lucarin@protonmail.com>
2024-04-30 10:20:54 +0200

    Replace neofetch by fastfetch

--- fish/config.fish
+++ fish/config.fish
@@ -7,5 +7,6 @@ alias 'pacman_clear'='yay -Qtdq | yay -Rns -'
 alias 'pacman_clear_show_all'='yay -Qqd | yay -Rsu --print -'
 alias 'pacman_clear_all'='yay -Qqd | yay -Rsu -'
 alias 'ssh'='kitten ssh'
+alias 'neofetch'='fastfetch'
 # Created by `pipx` on 2024-03-15 15:11:17
 set PATH $PATH /home/alex/.local/bin
