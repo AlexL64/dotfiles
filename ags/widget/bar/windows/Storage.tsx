8edd4b302087affd95f31f0c7b0c8e8256c07549
AlexL64 <github.qrfqa@slmails.com>
2025-11-13 23:39:05 +0100

    Only change height if child height is > above 0

--- ags/widget/bar/windows/Storage.tsx
+++ ags/widget/bar/windows/Storage.tsx
@@ -31,7 +31,8 @@ export default function Storage() {
                 devices.subscribe(() => {
                     const child_height = self.get_first_child()?.get_first_child()?.measure(Gtk.Orientation.VERTICAL, 999)[0];
 
-                    if (child_height != undefined) {
+                    if (child_height != undefined && child_height > 0) {
+                        print(child_height);
                         self.heightRequest = child_height;
                     }
                 })
