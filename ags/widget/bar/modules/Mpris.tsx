4c4364d71c3cb6a93696beac220ff12a411c9772
AlexL64 <github.qrfqa@slmails.com>
2025-11-24 21:41:35 +0100

    Fix Mpris play-pause button

--- ags/widget/bar/modules/Mpris.tsx
+++ ags/widget/bar/modules/Mpris.tsx
@@ -3,6 +3,7 @@ import { createBinding } from "gnim";
 import App from "ags/gtk4/app"
 import { Gdk, Gtk } from "ags/gtk4";
 import Pango from "gi://Pango?version=1.0";
+import { exec } from "ags/process";
 
 function lengthStr(length: number) {
     const min = Math.floor(length / 60)
@@ -36,7 +37,7 @@ export default function Mpris() {
         <button
             class={"play_pause"}
             cursor={Gdk.Cursor.new_from_name("pointer", null)}
-            onClicked={() => player.play_pause()}>
+            onClicked={() => exec(["bash", "-c", `playerctl play-pause`])}>
             <image iconName={playback_status} />
         </button>
         <button
