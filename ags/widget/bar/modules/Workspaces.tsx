0353dc75a9ce20de37aa52fe97c9adbdfe6641c7
AlexL64 <github.qrfqa@slmails.com>
2025-11-27 11:50:42 +0100

    Fix bug where the urgent workspace was removed when switching workspace

--- ags/widget/bar/modules/Workspaces.tsx
+++ ags/widget/bar/modules/Workspaces.tsx
@@ -22,8 +22,12 @@ export default function Workspaces(monitor: number) {
                     hyprland.get_workspace_by_name(i.toString())?.get_clients() != undefined ? self.add_css_class("active") : self.remove_css_class("active");
 
                     const focusedWorkspace = hyprland.connect("notify::focused-workspace", (hyprland) => {
-                        i == hyprland.focusedWorkspace.id ? self.add_css_class("focused") : self.remove_css_class("focused");
-                        self.remove_css_class("urgent");
+                        if (i == hyprland.focusedWorkspace.id) {
+                            self.remove_css_class("urgent");
+                            self.add_css_class("focused");
+                        } else {
+                            self.remove_css_class("focused");
+                        }
                     })
 
                     const urgent = hyprland.connect("urgent", (_, urgent) => {
