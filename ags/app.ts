e0f27fb5674be817c5c120866d6ef6b3175c8a80
AlexL64 <github.qrfqa@slmails.com>
2025-11-29 19:06:27 +0100

    Add Wallpapers to exceptions

--- ags/app.ts
+++ ags/app.ts
@@ -74,7 +74,7 @@ App.start({
 })
 
 App.connect("window-toggled", (_, window) => {
-    const exceptions = ["Bar", "Toast", "Clipboard", "Idle", "AppLauncher", "PowerSelector", "Monitors"];
+    const exceptions = ["Bar", "Toast", "Clipboard", "Idle", "AppLauncher", "PowerSelector", "Monitors", "Wallpapers"];
 
     if (exceptions.some(e => window.name.includes(e)) && !window.visible) {
         window.hide();
